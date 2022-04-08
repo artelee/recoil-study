fee61ff4c30c68a9d3d2d8abc5067be1d3c85368
artelee <eunhye4611@naver.com>
2022-04-08 15:38:46 +0900

    [todolist] 예제의 TodoList 따라하기

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import {RecoilRoot} from "recoil";
 import CharacterCounter from "./CharacterCounter";
+import {TodoList} from "./component/TodoList";
+
+// 가장 상단에 RecoilRoot가 있어야한다.
 
 function App() {
   return (
     <RecoilRoot>
-      <CharacterCounter/>
+      <TodoList/>
     </RecoilRoot>
   );
 }
