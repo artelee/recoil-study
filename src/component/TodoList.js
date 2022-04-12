59f43dea94e4db244020fda0c9f996dc631dd128
artelee <eunhye4611@naver.com>
2022-04-12 17:41:26 +0900

    [todolist] 예제의 TodoList 따라하기2

--- src/component/TodoList.js
+++ src/component/TodoList.js
@@ -1,4 +1,4 @@
-import {atom, useRecoilState, useRecoilValue, useSetRecoilState} from "recoil";
+import {atom, selector, useRecoilState, useRecoilValue, useSetRecoilState} from "recoil";
 import {useState} from "react";
 
 
@@ -6,17 +6,39 @@ const todoListState = atom({
     key: 'todoListState',
     default: [],
 });
+
+const todoListFilterState = atom({
+    key: 'todoListFilterState',
+    default: 'Show All',
+});
+const filteredTodoListState = selector({
+    key: 'filteredTodoListState',
+    get: ({get}) => {
+        const filter = get(todoListFilterState);
+        const list = get(todoListState);
+
+        switch (filter) {
+            case 'Show Completed':
+                return list.filter((item) => item.isComplete);
+            case 'Show Uncompleted':
+                return list.filter((item) => !item.isComplete);
+            default:
+                return list;
+        }
+    },
+});
+
 export function TodoList() {
     const todoList = useRecoilValue(todoListState);
 
     return (
         <>
-            {/* <TodoListStats /> */}
-            {/* <TodoListFilters /> */}
-            <TodoItemCreator />
+            <TodoListStats/>
+            <TodoListFilters/>
+            <TodoItemCreator/>
 
             {todoList.map((todoItem) => (
-                <TodoItem key={todoItem.id} item={todoItem} />
+                <TodoItem key={todoItem.id} item={todoItem}/>
             ))}
         </>
     );
@@ -45,7 +67,7 @@ function TodoItemCreator() {
 
     return (
         <div>
-            <input type="text" value={inputValue} onChange={onChange} />
+            <input type="text" value={inputValue} onChange={onChange}/>
             <button onClick={addItem}>Add</button>
         </div>
     );
@@ -53,6 +75,7 @@ function TodoItemCreator() {
 
 // 고유한 Id 생성을 위한 유틸리티
 let id = 0;
+
 function getId() {
     return id++;
 }
@@ -87,7 +110,7 @@ function TodoItem({item}) {
 
     return (
         <div>
-            <input type="text" value={item.text} onChange={editItemText} />
+            <input type="text" value={item.text} onChange={editItemText}/>
             <input
                 type="checkbox"
                 checked={item.isComplete}
@@ -104,4 +127,61 @@ function replaceItemAtIndex(arr, index, newValue) {
 
 function removeItemAtIndex(arr, index) {
     return [...arr.slice(0, index), ...arr.slice(index + 1)];
+}
+
+function TodoListFilters() {
+    const [filter, setFilter] = useRecoilState(todoListFilterState);
+
+    const updateFilter = ({target: {value}}) => {
+        setFilter(value);
+    };
+
+    return (
+        <>
+            Filter:
+            <select value={filter} onChange={updateFilter}>
+                <option value="Show All">All</option>
+                <option value="Show Completed">Completed</option>
+                <option value="Show Uncompleted">Uncompleted</option>
+            </select>
+        </>
+    );
+}
+
+const todoListStatsState = selector({
+    key: 'todoListStatsState',
+    get: ({get}) => {
+        const todoList = get(todoListState);
+        const totalNum = todoList.length;
+        const totalCompletedNum = todoList.filter((item) => item.isComplete).length;
+        const totalUncompletedNum = totalNum - totalCompletedNum;
+        const percentCompleted = totalNum === 0 ? 0 : totalCompletedNum / totalNum;
+
+        return {
+            totalNum,
+            totalCompletedNum,
+            totalUncompletedNum,
+            percentCompleted,
+        };
+    },
+});
+
+function TodoListStats() {
+    const {
+        totalNum,
+        totalCompletedNum,
+        totalUncompletedNum,
+        percentCompleted,
+    } = useRecoilValue(todoListStatsState);
+
+    const formattedPercentCompleted = Math.round(percentCompleted * 100);
+
+    return (
+        <ul>
+            <li>Total items: {totalNum}</li>
+            <li>Items completed: {totalCompletedNum}</li>
+            <li>Items not completed: {totalUncompletedNum}</li>
+            <li>Percent completed: {formattedPercentCompleted}</li>
+        </ul>
+    );
 }
