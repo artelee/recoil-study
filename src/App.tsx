6035c1416b2c7cebcf7542548954db4e977daa81
artelee <eunhye4611@naver.com>
2022-04-20 15:54:09 +0900

    [typescript] type으로 재 생성

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import {RecoilRoot,} from 'recoil';
+
+
+function App() {
+    return (
+        <RecoilRoot>
+            <div className="App">
+
+            </div>
+        </RecoilRoot>
+    );
+}
+
+export default App;
--- src/recoil/atom.ts
+++
@@ -1,8 +0,0 @@
-import { atom } from "recoil";
-
-const fontSizeState = atom({
-    key: 'fontSizeState',
-    default: 14,
-})
-
-export default fontSizeState
